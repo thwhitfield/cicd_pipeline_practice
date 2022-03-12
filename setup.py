b6846faa1c58de4c505806b34b29d0c0c6ad3f49
Travis Whitfield <thwhitfield@gmail.com>
2022-03-12 14:55:35 -0500

    Create my_pkg and initial files

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='cicd_pipeline_practice',
+    version = '0.0.1',
+    author = 'Travis Whitfield',
+    description = "CI/CD Pipeline Practice",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires = '>=3.7'
+)
