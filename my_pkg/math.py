f7b058a31fd069624c67105c7a333d50237be7dc
Travis Whitfield <thwhitfield@gmail.com>
2022-03-12 16:27:33 -0500

    Test pylint threshold=8

--- my_pkg/math.py
+++ my_pkg/math.py
@@ -7,11 +7,11 @@ def add(num1, num2):
 def subtract(num1, num2):
     """Subtract function."""
     return num1 - num2
-
+    
 def multiply(num1, num2):
     """Multiply function."""
     return num1 * num2
-
+    
 def divide(num1, num2):
     """Divide function."""
     return num1 / num2
