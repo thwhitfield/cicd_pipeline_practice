bf5724459369c8a22c6dea16ecb85504216420a2
Travis Whitfield <thwhitfield@gmail.com>
2022-03-12 16:02:13 -0500

    Update to pass pylint checks.

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,3 +1,5 @@
+"""Test toy math functions."""
+
 import pytest
 
 from my_pkg.math import add, subtract, multiply, divide
@@ -5,19 +7,23 @@ from my_pkg.math import add, subtract, multiply, divide
 
 @pytest.mark.unit
 def test_add():
+    """Test add function."""
     assert add(2, 3) == 5
 
 
 @pytest.mark.unit
 def test_subtract():
+    """Test subtract function."""
     assert subtract(5, 2) == 3
 
 
 @pytest.mark.unit
 def test_multiply():
+    """Test multiply function."""
     assert multiply(2, 4) == 8
 
 
 @pytest.mark.unit
 def test_divide():
+    """Test divide function."""
     assert divide(8, 2) == 4
