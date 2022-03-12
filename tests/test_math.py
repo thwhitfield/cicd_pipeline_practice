078c03f6cffcae07c248f088945239e2a757edd6
Travis Whitfield <thwhitfield@gmail.com>
2022-03-12 15:42:24 -0500

    Update CI/CD pipeline to use pylint

--- tests/test_math.py
+++ tests/test_math.py
@@ -2,14 +2,22 @@ import pytest
 
 from my_pkg.math import add, subtract, multiply, divide
 
+
+@pytest.mark.unit
 def test_add():
     assert add(2, 3) == 5
-    
+
+
+@pytest.mark.unit
 def test_subtract():
     assert subtract(5, 2) == 3
-    
+
+
+@pytest.mark.unit
 def test_multiply():
     assert multiply(2, 4) == 8
-    
+
+
+@pytest.mark.unit
 def test_divide():
     assert divide(8, 2) == 4
